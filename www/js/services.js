6e06602e2eb2fed5a90916120c0c16c6fab3188f
seekdestiny <jifeiqian@gmail.com>
2016-01-23 22:28:46 -0800

    updating the services.js

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('confusionApp')
+angular.module('confusion.services', ['ngResouce'])
         .constant("baseURL","http://localhost:3000/")
         .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
     
