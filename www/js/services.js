57896318c0a24586c5fe89ccf1c42501bfc7b5b5
seekdestiny <jifeiqian@gmail.com>
2016-01-23 22:59:46 -0800

    show home page information

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('confusion.services', ['ngResouce'])
+angular.module('conFusion.services', ['ngResource'])
         .constant("baseURL","http://localhost:3000/")
         .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
     
