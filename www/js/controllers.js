8efb658cc480630213c695d846def4d152ee6270
seekdestiny <jifeiqian@gmail.com>
2016-01-26 21:10:16 -0800

    adding favoritesController

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -228,4 +228,34 @@ angular.module('conFusion.controllers', [])
 				}
 			);
          }])
+
+		.controller('FavoritesController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
+
+    $scope.baseURL = baseURL;
+    $scope.shouldShowDelete = false;
+
+    $scope.favorites = favoriteFactory.getFavorites();
+
+    $scope.dishes = menuFactory.getDishes().query (
+        function (response) {
+            $scope.dishes = response;
+        },
+        function (response) {
+            $scope.message = "Error: " + response.status + " " + response.statusText;
+        }
+	);
+    console.log($scope.dishes, $scope.favorites);
+
+    $scope.toggleDelete = function () {
+        $scope.shouldShowDelete = !$scope.shouldShowDelete;
+        console.log($scope.shouldShowDelete);
+    };
+
+    $scope.deleteFavorite = function (index) {
+        
+        favoriteFactory.deleteFromFavorites(index);
+        $scope.shouldShowDelete = false;
+
+    };
+	}])
 ;
